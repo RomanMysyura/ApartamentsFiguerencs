6b733d465b60fd10d1fc99790e73cdc23179f322
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-10 17:11:01 +0200

    prova git

---
+++ frontend/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apartaments Figuerencs</title>
+    <link rel="icon" href="imatges/ApartamentsFiguerencs.png" type="image/x-icon">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="login.js"></script>
+</head>
+<body>
+    <header class="bg-primary-subtle">
+        <div class="container d-flex justify-content-between align-items-center py-3">
+            <div>
+                <a href="index.php">
+                    <img src="imatges/ApartamentsFiguerencs.png" alt="Logo" width="50" height="50">
+                </a>
+               
+            </div>
+            <div>
+                <button type="button" class="btn btn-info mx-2"  onclick="window.location.href='reservar.php'">Reservar</button>
+                <button type="button" class="btn btn-info mx-2">Contactar</button>
+                <button type="button" class="btn btn-info mx-2">Ubicació</button>
+            </div>
+            <div>
+            <button type="button" class="btnlogin btn btn-primary" id="BotonLogin" onclick="window.location.href='login.php'">Entrar/Registrar</button>
+
+            </div>
+        </div>
+    </header>
+
+    <h1>12345678910</h1>
+
+    <div class="container ">
+      <h2 class="titolpagina">Luxe i confort ben a prop teu!</h2>
+      
+    </div>
+    <div class="input-group divbuscar">
+            <input type="search" class="form-control rounded formularibuscar" placeholder="Buscar els meus apartaments" aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn btn-outline-primary buttonbuscar">Buscar</button>
+          </div>
+          
+    
+
+
+
+   
+    
+
+
+
+
+
+
+<div>
+
+</div>
+</body>
+</html>

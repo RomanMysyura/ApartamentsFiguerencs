93815c8ebcf3f6c2dc3c265b0a61bbac09aa2d2b
Adrian Poncelas Juncarol <aponcelas@cendrassos.net>
2023-10-16 18:29:23 +0200

    Versió 1 APJ

--- src/views/login.php
+++ src/views/login.php
@@ -1,26 +1,49 @@
+<?php 
+session_start();
+
+$db = new PDO("mysql:host=localhost;dbname=nombre_de_la_base_de_datos", "usuario_db", "contraseña_db");
+
+$usename = $_POST['username'];
+$password = $_POST['password'];
+
+$stmt = $db->prepare("SELECT * FROM users WHERE user = ?");
+$stmt->execute(['username']);
+$user = $stmt->fetch();
+
+if ($user && password_verify($password, $user['password'])) {
+  // Autenticación exitosa
+  $_SESSION['username'] = $username;
+  header("Location: inicio_exitoso.php");
+} else {
+  // Autenticación fallida
+  header("Location: inicio_fallido.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Apartaments Figuerencs</title>
+    <link rel="stylesheet" href="login.css">
     <link rel="icon" href="imatges/ApartamentsFiguerencs.png" type="image/x-icon">
-    <link rel="stylesheet" href="index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="login.js"></script>
 </head>
-<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+<body class="p-3 m-0 border-0 bd-example m-0 border-10">
+<div class="position-absolute top-50 start-50 translate-middle shadow p-3 mb-5 bg-white rounded">
     <!-- Example Code -->
-
-    <form>
+    <form method = "POST">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Nom d'usuari</label>
         <input
           type="text"
           class="form-control"
-          id="exampleInputEmail1"
+          id="username"
           aria-describedby="emailHelp"
         />
       </div>
@@ -31,7 +54,7 @@
         <input
           type="password"
           class="form-control"
-          id="exampleInputPassword1"
+          id="password"
         />
       </div>
       <div class="mb-3 form-check">
@@ -42,10 +65,10 @@
       </div>
       <button type="submit" class="btn btn-primary">Iniciar sessió</button>
       <div class="mb-3">
-        <p>No tens un compte creat? <a href="#">Registrat</a></p>
+        <p>No tens un compte creat? <a href="signup.php">Registrat</a></p>
       </div>
     </form>
-
+</div>
     <!-- End Example Code -->
   </body>
 </html>
